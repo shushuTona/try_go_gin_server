9f15337ad512674e038512ef03977f4c466de91f
shushuTona <42406967+shushuTona@users.noreply.github.com>
2023-02-08 02:50:14 +0900

    add nginx container

--- src/main.go
+++ src/main.go
@@ -5,7 +5,7 @@ import "github.com/gin-gonic/gin"
 func setupRouter() *gin.Engine {
 	var router = gin.Default()
 
-	router.GET("/ping", func(c *gin.Context) {
+	router.GET("/api/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
